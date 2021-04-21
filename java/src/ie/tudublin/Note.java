12ae0093de7c4cb10589c8a03dbb54ceea172efe
LinaM20 <c19366191@mytudublin.ie>
2021-04-21 16:16:20 +0100

    Created a toString method and added comments

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -1,12 +1,21 @@
 package ie.tudublin;
 
 public class Note {
+    //private fields
     private char note;
     private int duration;
 
+    //constructor
     public Note(char note, int duration)
     {
         this.note = note;
         this.duration = duration;
     }
+
+    //toString method
+    public String toString() {
+        return "Notes [note= " + note + "length of note = " + duration + "]";
+
+    }
+
 }
