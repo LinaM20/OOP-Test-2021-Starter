e5cd2fb60335ea087b4c0d884599f7cbcf331963
LinaM20 <c19366191@mytudublin.ie>
2021-04-21 16:12:44 +0100

    Created a constructor

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,12 @@
+package ie.tudublin;
+
+public class Note {
+    private char note;
+    private int duration;
+
+    public Note(char note, int duration)
+    {
+        this.note = note;
+        this.duration = duration;
+    }
+}
